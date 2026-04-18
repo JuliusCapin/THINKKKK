dd035dd8dd5ee4f90d623a928da5ab088374b09d
JuliusCapin <143344694+JuliusCapin@users.noreply.github.com>
2026-04-18 14:34:15 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,23 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 const quizRoute = require('./routes/quiz');
+const authRoute = require('./routes/auth');
 
 const app = express();
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type']
-}));
-
+app.use(cors({ origin: '*', methods: ['GET', 'POST'], allowedHeaders: ['Content-Type', 'Authorization'] }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('/api', quizRoute);
+app.use('/api/auth', authRoute);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running at \x1b]8;;http://localhost:${PORT}\x07http://localhost:${PORT}\x1b]8;;\x07`));
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+    })
+    .catch(err => console.error('MongoDB connection error:', err));
